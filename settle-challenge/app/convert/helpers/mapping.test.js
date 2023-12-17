8efcf8101f63f7e96a138b25bb3f565fb7313b7c
AugustoGodoy <augustog5896@gmail.com>
2023-12-17 15:12:00 -0300

    Add convertedAmountSendMerchantMapping function to mapping.test.js

--- settle-challenge/app/convert/helpers/mapping.test.js
+++ settle-challenge/app/convert/helpers/mapping.test.js
@@ -1,4 +1,5 @@
 const { convertedAmountSendMapping } = require('./mapping');
+const { convertedAmountSendMerchantMapping } = require('./mapping');
 
 describe('convertedAmountSendMapping', () => {
   it('should return the mapped object with converted amount rounded to 2 decimals', () => {
@@ -54,6 +55,30 @@ describe('convertedAmountSendMapping', () => {
     };
 
     const result = convertedAmountSendMapping(converted);
+
+    expect(result).toEqual(expected);
+  });
+});
+
+describe('convertedAmountSendMerchantMapping', () => {
+  it('should return the mapped object with converted amount rounded to 2 decimals', () => {
+    const converted = {
+      _id: '123',
+      pair: 'USDEUR',
+      originalAmount: 100,
+      convertedAmount: 91.681234,
+      createAt: '2022-01-01',
+    };
+
+    const expected = {
+      id: '123',
+      pair: 'USDEUR',
+      originalAmount: 100,
+      convertedAmount: 91.68,
+      createAt: '2022-01-01',
+    };
+
+    const result = convertedAmountSendMerchantMapping(converted);
 
     expect(result).toEqual(expected);
   });
